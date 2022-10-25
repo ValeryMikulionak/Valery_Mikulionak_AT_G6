7e1d2523815a09cfd0c33619f90a30709f649609
Valery Mikulionak <valery.mikulionak@gmail.com>
2022-10-25 13:57:57 +0300

    I have finished 4-5 task from ArrayList

--- src/main/java/homework/day8/Cars.java
+++ src/main/java/homework/day8/Cars.java
@@ -1,9 +1,40 @@
 package homework.day8;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cars {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        List<String> cars = new ArrayList<>();
+
+        cars.add("Мерс");
+        cars.add("Ауди");
+        cars.add("Жигуль");
+        cars.add("Рено");
+        cars.add("Жигуль");
+        cars.add("Жигуль");
+        cars.add("Ауди");
+
+        BufferedWriter out = new BufferedWriter(new FileWriter("cars.txt"));
+        for (String str : cars) {
+            out.write("\"" + str + "\"" + "\n");
+        }
+        out.close();
+
+        for (String str : cars) {
+            if (str.length() <= 4) {
+                System.out.println(str);
+            }
+        }
+        System.out.println();
+
+        for (String str : cars) {
+            System.out.printf(str + " ");
+        }
     }
-
 }
